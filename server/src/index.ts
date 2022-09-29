8fffdc89ce2c4fd53e7b720297cc2a4c24112e18
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-29 10:58:05 -0300

    DB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import { schema } from "./Schema";
 import cors from "cors";
 import { createConnection } from "typeorm";
 
@@ -17,10 +18,10 @@ const main = async () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
-  // app.use("/graphql", graphqlHTTP({
-  //     schema,
-  //     graphiql: true
-  // }))
+  app.use("/graphql", graphqlHTTP({
+      schema,
+      graphiql: true
+  }))
 
   const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
