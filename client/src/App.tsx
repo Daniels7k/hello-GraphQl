f6e4997d18246ba55a5ffa5f47494600df5d0049
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-03 16:50:07 -0300

    createUser component created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
 import "./App.css";
-import { useState } from "react";
+import CreateUser from "./components/CreateUser";
 
 function App() {
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
+  
   const client = new ApolloClient({
     uri: "http://localhost:3001/graphql",
     cache: new InMemoryCache(),
@@ -15,35 +18,7 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <div className="createUser">
-        <input
-          type="text"
-          placeholder="name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-
-        <input
-          type="text"
-          placeholder="username"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-
-        <input
-          type="text"
-          placeholder="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button>Create User</button>
-      </div>
+      <CreateUser />
     </ApolloProvider>
   );
 }
