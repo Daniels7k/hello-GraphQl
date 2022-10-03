65d1aa8eada38e0268234c829ad14c15083f71c7
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-03 09:03:15 -0300

    User Mutation database

--- server/src/Schema/Mutations/User.ts
+++ server/src/Schema/Mutations/User.ts
@@ -1,4 +1,5 @@
 import { GraphQLString } from "graphql";
+import { Users } from "../../Entities/Users";
 import { UserType } from "../TypeDefs/User";
 
 export const CREATE_USER = {
@@ -8,8 +9,9 @@ export const CREATE_USER = {
     username: { type: GraphQLString },
     password: { type: GraphQLString },
   },
-  resolve(parent: any, args: any) {
+  async resolve(parent: any, args: any) {
     const { name, username, password } = args;
+    await Users.insert({name, username, password})
     return args;
   },
 };
