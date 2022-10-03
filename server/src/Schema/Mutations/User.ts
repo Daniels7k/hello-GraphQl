20d24c11e608ecdb1ba2dcb8f5a5af7219819fda
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-03 14:18:16 -0300

    Update password mutation

--- server/src/Schema/Mutations/User.ts
+++ server/src/Schema/Mutations/User.ts
@@ -11,7 +11,7 @@ export const CREATE_USER = {
   },
   async resolve(parent: any, args: any) {
     const { name, username, password } = args;
-    await Users.insert({name, username, password})
+    await Users.insert({ name, username, password });
     return args;
   },
 };
@@ -19,11 +19,34 @@ export const CREATE_USER = {
 export const DELETE_USER = {
   type: UserType,
   args: {
-    id: { type: GraphQLID }
+    id: { type: GraphQLID },
   },
   async resolve(parent: any, args: any) {
     const { id } = args;
-    await Users.delete(id)
+    await Users.delete(id);
     return args;
+  },
+};
+
+export const UPDATE_PASSWORD = {
+  type: UserType,
+  args: {
+    username: { type: GraphQLString },
+    oldPassword: { type: GraphQLString },
+    newPassword: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { username, oldPassword, newPassword } = args;
+    const user = await Users.findOne({ where: { username: username } });
+    const userPassword = user?.password;
+    console.log(user)
+    if (oldPassword === userPassword) {
+      return await Users.update(
+        { username: username },
+        { password: newPassword }
+      );
+    } else {
+      throw new Error("PASSWORDS DO NOT MATCH");
+    }
   },
 };
