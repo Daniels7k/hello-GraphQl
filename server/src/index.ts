8b108366cebab0bf55f41194eb124488acabdd48
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-03 08:52:44 -0300

    User database request

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import { graphqlHTTP } from "express-graphql";
 import { schema } from "./Schema";
 import cors from "cors";
 import { createConnection } from "typeorm";
+import { Users } from "./Entities/Users";
 
 const main = async () => {
   await createConnection({
@@ -12,7 +13,7 @@ const main = async () => {
     password: "password",
     logging: true,
     synchronize: false,
-    entities: [],
+    entities: [Users],
   });
 
   const app = express();
